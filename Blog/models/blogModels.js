d10240f291d1dd3b7e6b85431926f75a5ceacd8d
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-10-03 20:38:36 +0545

    push

---
+++ Blog/models/blogModels.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    author: {type: String, required: true},
+    date: {type: Date, default: Date.now},
+    description: {type: String, required: true},
+    image: {type: String},
+}, {timestamps: true});
+
+const Blog = mongoose.model("Blog", blogSchema);
+export default Blog;
